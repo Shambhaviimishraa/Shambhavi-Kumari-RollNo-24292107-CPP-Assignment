c6390b5bfaea8d291b66be81d2dfee5e273dac31
Shambhaviimishraa <mishrashambhavi783@gmail.com>
2025-12-28 15:32:11 +0530

    Add program to display sizes of basic data types

---
+++ Module-1-Basics/Q2_BasicStructures&DataTypes.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+  cout<<"size of char:"<<size of (char)<<"bytes"<<endl;
+  cout<<"size of int:"<<size of (int)<<"bytes"<<endl;
+  cout<<"size of float:"<<size of (float)<<"bytes"<<endl;
+  cout<<"size of double:"<<size of (double)<<"bytes"<<endl;
+  cout<<"size of bool:"<<size of (char)<<"bool"<<endl;
+return 0;
+}
+
