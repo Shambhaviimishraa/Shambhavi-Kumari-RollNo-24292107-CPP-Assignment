2d18825fed7bc11789c09a6fe82f13a4352f3861
Shambhaviimishraa <mishrashambhavi783@gmail.com>
2025-12-28 15:51:05 +0530

    Create Q5_DataTypes.cpp

---
+++ Module-1-Basics/Q5_DataTypes.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  const int marks = 100;   // constant variable
+
+    cout << "Maximum marks are: " << marks << endl;
+
+    return 0;
+}
