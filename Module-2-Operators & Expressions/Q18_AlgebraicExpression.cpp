7f36b782f109402622d045ef41fcea5304d2c3e0
Shambhaviimishraa <mishrashambhavi783@gmail.com>
2025-12-29 21:26:51 +0530

    Modify algebraic expression calculation in main
    Updated the main function to calculate a different expression.

---
+++ Module-2-Operators & Expressions/Q18_AlgebraicExpression.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a, b, c;
+
+    cout << "Enter value of a and b: ";
+    cin >> a >> b;
+
+    c = (a * a) + (b * b);
+
+    cout << "Value of the expression is: " << c << endl;
+
+    return 0;
+}
