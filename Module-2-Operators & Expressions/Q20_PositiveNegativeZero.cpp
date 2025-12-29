37d158699eaa153b694f9c6f31ea3f54c7ed8ddf
Shambhaviimishraa <mishrashambhavi783@gmail.com>
2025-12-29 21:40:46 +0530

    Create Q20_PositiveNegativeZero.cpp

---
+++ Module-2-Operators & Expressions/Q20_PositiveNegativeZero.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  #include <iostream>
+using namespace std;
+
+int main() 
+{
+    float num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num > 0) {
+        cout << "The number is Positive";
+    } else if (num < 0) {
+        cout << "The number is Negative";
+    } else {
+        cout << "The number is Zero";
+    }
+
+    return 0;
+}
