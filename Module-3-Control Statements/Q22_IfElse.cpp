01a054e335837c9fee0ba05ef5a2670d1f24837e
Shambhaviimishraa <mishrashambhavi783@gmail.com>
2025-12-30 21:31:26 +0530

    Add age check for driving eligibility

---
+++ Module-3-Control Statements/Q22_IfElse.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int age = 17;
+
+if(age == 18) {
+cout<<"YOU CAN DRIVE"<<endl;
+}
+else{
+cout<<"YOU CANNOT DRIVE"<<endl;
+}
+
+return 0;
+}
