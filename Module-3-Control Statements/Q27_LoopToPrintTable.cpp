55ab35f365c1aaced42a3e1375b89380c175ff5c
Shambhaviimishraa <mishrashambhavi783@gmail.com>
2025-12-30 21:54:57 +0530

    Create Q27_LoopToPrintTable.cpp

---
+++ Module-3-Control Statements/Q27_LoopToPrintTable.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number, i = 1;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    while (i <= 10)
+    {
+        cout << number << " x " << i << " = " << number * i << endl;
+        i++;
+    }
+
+    return 0;
+}
