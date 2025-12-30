eaea0b848ff33da021af3e02c60e18a99e13698b
Shambhaviimishraa <mishrashambhavi783@gmail.com>
2025-12-30 21:52:46 +0530

    Create Q26_Loop.cpp

---
+++ Module-3-Control Statements/Q26_Loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+
+    cout << "Number series from 1 to 10:" << endl;
+
+    for (i = 1; i <= 10; i++)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
